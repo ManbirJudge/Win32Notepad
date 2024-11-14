24e96f7d326010609cead10f0bce9ecd7dd84050
Manbir Judge <manbirjudge2009@gmail.com>
2024-11-14 20:29:00 +0530

    Cleanup

--- notepad/resource.h
+++ notepad/resource.h
@@ -8,6 +8,8 @@
 #define IDR_MAINFRAME                   128
 #define IDD_FIND                        129
 #define IDR_MAINMENU                    130
+#define IDI_ICON1                       131
+#define IDI_APPLICATION                 131
 #define IDC_FIND_TXT                    1001
 #define IDC_FIND_NXT                    1002
 #define IDC_FIND_CANCEL                 1003
@@ -45,7 +47,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32790
 #define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           110
